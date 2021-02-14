3f0efa26a2d39e45796c1a9d3e7a5b310ea0d00d
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2021-02-14 15:58:43 -0600

    progress

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,26 @@
+// store our api endpoint inside queryurl
+var queryurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryurl, function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h3>" + feature.geometry.coordinates + "</h3>")
+    };
+  
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
+  
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+  };
