128b016133688c7f1def27ddb9421b362269a154
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2021-02-20 09:58:40 -0600

    progress

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,9 @@
 // store our api endpoint inside queryurl
-var queryurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+var queryurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-// Perform a GET request to the query URL
+// perform a GET request to the query URL
 d3.json(queryurl, function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function
+    // once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
 });
 
@@ -17,19 +17,55 @@ function createFeatures(earthquakeData) {
     }
 
 
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
+    // create a GeoJSON layer containing the features array on the earthquakeData object
+    // run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature
     });
 
-    // Sending our earthquakes layer to the createMap function
+    // sending our earthquakes layer to the createMap function
     createMap(earthquakes);
 };
 
+
+// define arrays to hold marker information for each feature
+var magnitudeMarkers = [];
+var depthMarkers = [];
+
+// loop through earthquake data and create depth and magnitude markers
+for (var i = 0; i < earthquakedata.features.length; i++) {
+  // setting the marker radius for the depth of the earthquake by passing coordinates into the markerSize function
+  depthMarkers.push(
+    L.circle(earthquakedata[i].geometry.coordinates[2], {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: "white",
+      radius: markerSize(earthquakedata[i].geometry.coordinates[2])
+    })
+  );
+
+  // setting the marker radius for the magnitude by passing magnitude property into the markerSize function
+  magnitudeMarkers.push(
+    L.circle(earthquakedata[i].geometry.coordinates[2], {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "purple",
+      fillColor: "purple",
+      radius: markerSize(earthquakedata[i].features.properties.mag)
+    })
+  );
+}
+
+
+
+
+
+
+
 function createMap(earthquakes) {
 
-    // Define streetmap and darkmap layers
+    // define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
@@ -46,18 +82,25 @@ function createMap(earthquakes) {
         accessToken: API_KEY
     });
 
-    // Define a baseMaps object to hold our base layers
+    // define a baseMaps object to hold our base layers
     var baseMaps = {
         "Street Map": streetmap,
         "Dark Map": darkmap
     };
 
-    // Create overlay object to hold our overlay layer
+    // Create two separate layer groups: one for cities and one for states
+    var depth = L.layerGroup(depthMarkers);
+    var magnitude = L.layerGroup(magnitudeMarkers);
+
+
+    // create overlay object to hold our overlay layer
     var overlayMaps = {
-        Earthquakes: earthquakes
+        Earthquakes: earthquakes,
+        Depth: depth,
+        Magnitude: magnitude
     };
 
-    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    // create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("mapid", {
         center: [
             37.09, -95.71
@@ -66,9 +109,9 @@ function createMap(earthquakes) {
         layers: [streetmap, earthquakes]
     });
 
-    // Create a layer control
-    // Pass in our baseMaps and overlayMaps
-    // Add the layer control to the map
+    // create a layer control
+    // pass in our baseMaps and overlayMaps
+    // add the layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
