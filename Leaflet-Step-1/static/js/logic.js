510108929a005228235de5c5dc6bc5ddcc287d54
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2021-02-20 18:28:34 -0600

    markers complete need to fix legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -9,13 +9,12 @@ d3.json(queryurl, function (data) {
 
 function createFeatures(earthquakeData) {
 
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
+    // define a function we want to run once for each feature in the features array
+    // give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place +
             "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
     }
-
 
     // create a GeoJSON layer containing the features array on the earthquakeData object
     // run the onEachFeature function once for each piece of data in the array
@@ -23,15 +22,28 @@ function createFeatures(earthquakeData) {
         onEachFeature: onEachFeature
     });
 
-    // sending our earthquakes layer to the createMap function
-    createMap(earthquakes, depthMarkers, magnitudeMarkers);
-
-
+    // create a function for the marker size of the circles
     function markerSize(size) {
         // console.log(size)
-        return size * 4;
-      }
+        return size * 40000;
+    };
 
+    // create a function for the marker color of the circles
+    function markerColor(depth) {
+        if (depth < 10) {
+            return "#71FF33"
+        } else if (depth < 30){
+            return "#E9FF33"
+        } else if (depth < 50){
+            return "#FFC133"
+        } else if (depth < 70){
+            return "#FF8D33"
+        } else if (depth < 100){
+            return "#FF3933"
+        } else if (depth < 150){
+            return "#F90E07"
+        }
+    };
 
     // define arrays to hold marker information for each feature
     var magnitudeMarkers = [];
@@ -39,29 +51,19 @@ function createFeatures(earthquakeData) {
 
     // loop through earthquake data and create depth and magnitude markers
     for (var i = 0; i < earthquakeData.length; i++) {
-        // setting the marker radius for the depth of the earthquake by passing coordinates into the markerSize function
-        // depthMarkers.push(
-        //     L.circle(earthquakeData[i].geometry.coordinates[2], {
-        //         stroke: false,
-        //         fillOpacity: 0.75,
-        //         color: "white",
-        //         fillColor: "white",
-        //         radius: markerSize(earthquakeData[i].geometry.coordinates[2])
-        //     })
-        // );
 
         // setting the marker radius for the magnitude by passing magnitude property into the markerSize function
         magnitudeMarkers.push(
-            L.circle(earthquakeData[i].geometry.coordinates[2], {
+            L.circle([earthquakeData[i].geometry.coordinates[1], earthquakeData[i].geometry.coordinates[0]], {
                 stroke: false,
                 fillOpacity: 0.75,
                 color: "purple",
-                fillColor: "purple",
+                fillColor: markerColor(earthquakeData[i].geometry.coordinates[2]),
                 radius: markerSize(earthquakeData[i].properties.mag)
             })
         );
     }
-
+    createMap(earthquakes, magnitudeMarkers);
 };
 
 
@@ -96,7 +98,9 @@ function createMap(earthquakes, magnitudeMarkers) {
     // Create two separate layer groups: one for depth and one for magnitude
     // var depth = L.layerGroup(depthMarkers);
     var magnitude = L.layerGroup(magnitudeMarkers);
-
+    console.log(magnitudeMarkers)
+    // magnitude.addTo(myMap);
+    // console.log("here i am", magnitude)
 
     // create overlay object to hold our overlay layer
     var overlayMaps = {
@@ -105,14 +109,55 @@ function createMap(earthquakes, magnitudeMarkers) {
         Magnitude: magnitude
     };
 
+
+
+
+    console.log(magnitude)
     // create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("mapid", {
         center: [
             37.09, -95.71
         ],
         zoom: 5,
-        layers: [streetmap, earthquakes]
+        layers: [streetmap, earthquakes, magnitude]
     });
+
+
+    // creating the legend to show the colors coordinating to depth of each earthquake
+    var legend = L.control({
+        position: "bottomright"
+      });
+
+      // creating a div to hold the legend
+      legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+
+        // create ranges and colors variables correlating to the markers
+        var ranges = [-10, 10, 30, 50, 70, 90];
+        var colors = [
+          "#71FF33",
+          "#E9FF33",
+          "#FFC133",
+          "#FF8D33",
+          "#FF3933",
+          "#F90E07"];
+        // loop through each range and create a label for the legend.
+        for (var i = 0; i < ranges.length-1; i++) {
+          div.innerHTML += "<i style='background: "
+            + colors[i]
+            + "'></i> "
+            + ranges[i]
+            + "-" + ranges[i + 1] + "<br>"  
+        }
+        console.log(div.innerHTML)
+        return div;
+      };
+      // add legend to the map.
+      legend.addTo(myMap);
+
+
+
+
 
     // create a layer control
     // pass in our baseMaps and overlayMaps
